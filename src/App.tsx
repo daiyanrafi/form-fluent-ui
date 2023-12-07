a480c6f0f1d6f604d19f2c5ed20869fc64091d09
Raf <daiyanrafi1@gmail.com>
2023-12-07 13:44:11 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import MyChoiceGroup from './components/ChoiceGroup/ChoiceGroup';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="content-container">
+      <MyChoiceGroup />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/ChoiceGroup/ChoiceGroup.tsx
@@ -0,0 +1,87 @@
+import React, { useState } from 'react';
+import { ChoiceGroup, IChoiceGroupOption, DefaultButton } from '@fluentui/react';
+import './ChoiceGroup.css';
+
+const MyChoiceGroup: React.FC = () => {
+  const [selectedOption1, setSelectedOption1] = useState<string | undefined>('yes1');
+  const [selectedOption2, setSelectedOption2] = useState<string | undefined>('yes2');
+  const [showWarning, setShowWarning] = useState(false);
+  const [showOption2, setShowOption2] = useState(false);
+
+  const options1: IChoiceGroupOption[] = [
+    { key: 'yes1', text: 'Yes', name: 'group1' },
+    { key: 'no1', text: 'No', name: 'group1' },
+  ];
+
+  const options2: IChoiceGroupOption[] = [
+    { key: 'yes2', text: 'Yes', name: 'group2' },
+    { key: 'no2', text: 'No', name: 'group2' },
+  ];
+
+  const onChange1 = (_ev?: React.FormEvent<HTMLElement | HTMLInputElement>, option?: IChoiceGroupOption): void => {
+    if (option) {
+      setSelectedOption1(option.key);
+      // Reset the state for the second choice group and warning
+      setShowWarning(false);
+      setSelectedOption2(undefined);
+      setShowOption2(false);
+
+      // Handle logic based on the selected option in the first group
+      if (option.key === 'yes1') {
+        setShowWarning(true);
+      } else {
+        setShowOption2(true);
+      }
+    }
+  };
+
+  const onChange2 = (_ev?: React.FormEvent<HTMLElement | HTMLInputElement>, option?: IChoiceGroupOption): void => {
+    if (option) {
+      setSelectedOption2(option.key);
+      // Reset the state for the warning
+      setShowWarning(false);
+
+      // Handle logic based on the selected option in the second group
+      if (option.key === 'yes2') {
+        // You can perform additional logic here if needed
+      }
+    }
+  };
+
+  const handleAddButtonClick = (): void => {
+    console.log('Add button clicked');
+  };
+
+  return (
+    <div className="choice-container">
+      <div className="title">Questionnarie:</div>
+      <ChoiceGroup
+        className="choice-group"
+        selectedKey={selectedOption1}
+        options={options1}
+        onChange={onChange1}
+        label="Are you without electricity, gas or water?"
+        required
+      />
+      {showWarning && <div className="warning">Please contact with your foreman +376423926719</div>}
+      {showOption2 && (
+        <>
+          <ChoiceGroup
+            className="choice-group"
+            selectedKey={selectedOption2}
+            options={options2}
+            onChange={onChange2}
+            label="Have you contacted your provider to try to resolve your complaint?"
+            required
+          />
+          {selectedOption2 === 'no2' && <div className="warning">Please contact with your foreman +00393946334</div>}
+          {selectedOption2 === 'yes2' && (
+            <DefaultButton className="add-button" onClick={handleAddButtonClick} text="Add" />
+          )}
+        </>
+      )}
+    </div>
+  );
+};
+
+export default MyChoiceGroup;
